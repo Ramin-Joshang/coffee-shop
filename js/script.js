65cabc980d581f2f30b8d58d71df12bad94df419
Ramin Joshang <rjwshng@gmail.com>
2021-11-22 22:49:12 +0330

    Site responsive completed and fixed some bugs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
+// navbar item
 var ul = document.querySelector('.header-list'); // Parent
-console.log(ul);
 ul.addEventListener('click', function(e) {
     const listItem = document.querySelectorAll(".list-item");
     for (let i = 0; i < listItem.length; i++) {
         listItem[i].classList.remove("active");
 
     }
-
     e.target.classList.add("active");
 });
+
+// responsive navbar
+
+const navbarList = document.querySelector(".header-list");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const closeBtn = document.querySelector(".closeBtn");
+const body = document.getElementsByTagName("body");
+
+// open and close menu when user click on hamburgerBtn
+hamburgerBtn.addEventListener("click", () => {
+    navbarList.classList.add("showHide");
+    closeBtn.style.display = "block"
+    body[0].style.overflow = "hidden";
+});
+
+// close menu when user click on windows
+closeBtn.addEventListener("click", () => {
+    navbarList.classList.remove("showHide");
+    closeBtn.style.display = "none"
+    body[0].style.overflow = "auto";
+
+})
